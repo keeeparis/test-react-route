4b2f67f664e904443244c999cc094995a25e404f
Vladimir Trotsenko @keeeparis <vladimir1trotsenko@gmail.com>
2021-12-10 14:34:29 +0600

    a

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:keeeparis/test-react-route.git main:gh-pages
+git push -f git@github.com:keeeparis/react-route-test.git main:gh-pages
 
 cd -
